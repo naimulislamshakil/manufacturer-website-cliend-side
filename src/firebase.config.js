3877e047e1f380891234e70cecb243abe485b07a
naimulislamshakil <ms0413354@gmail.com>
2022-05-23 22:46:37 +0600

    add firebase

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -6,12 +6,19 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.FB_APIKEY,
-  authDomain: process.env.FB_AUTHDOMAIN,
-  projectId: process.env.FB_PROJECTID,
-  storageBucket: process.env.FB_STORAGEBUCKET,
-  messagingSenderId: process.env.FB_MESSAGINGSENDERID,
-  appId: process.env.FB_APPID,
+  // apiKey: "AIzaSyCYOZ726d7wLfDg4TshuqVdJCn1bRgEnxU",
+  // authDomain: "build-laptop-parts.firebaseapp.com",
+  // projectId: "build-laptop-parts",
+  // storageBucket: "build-laptop-parts.appspot.com",
+  // messagingSenderId: "1055432287495",
+  // appId: "1:1055432287495:web:47d8b12c6162f7ce4e51a2",
+
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 // Initialize Firebase
