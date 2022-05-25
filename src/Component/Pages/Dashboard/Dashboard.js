d7988d8f73a724e22edbd89b607aca1d54e8f251
naimulislamshakil <ms0413354@gmail.com>
2022-05-25 13:24:08 +0600

    create use

--- src/Component/Pages/Dashboard/Dashboard.js
+++ src/Component/Pages/Dashboard/Dashboard.js
@@ -1,7 +1,17 @@
 import React from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
 import { Link, Outlet } from "react-router-dom";
+import auth from "../../../firebase.config";
+import useAdmin from "../../Hooks/useAdmin";
+import Loading from "../../Shered/Loading/Loading";
 
 const Dashboard = () => {
+  const [user] = useAuthState(auth);
+  const [admin, isLoading] = useAdmin(user);
+
+  if (isLoading) {
+    return <Loading></Loading>;
+  }
   return (
     <div class="drawer drawer-mobile">
       <input id="open" type="checkbox" class="drawer-toggle" />
@@ -16,22 +26,27 @@ const Dashboard = () => {
         <label for="open" class="drawer-overlay"></label>
         <ul class="menu p-4 overflow-y-auto w-80 bg-secondary text-base-content">
           {/* <!-- Sidebar content here --> */}
-          <li>
-            <Link
-              className="w-full btn btn-outline rounded-full mt-3"
-              to="/dashboard "
-            >
-              My Orders
-            </Link>
-          </li>
-          <li>
-            <Link
-              className="w-full btn btn-outline rounded-full mt-3"
-              to="/dashboard/add_review"
-            >
-              Add A Review
-            </Link>
-          </li>
+          {!admin && (
+            <>
+              <li>
+                <Link
+                  className="w-full btn btn-outline rounded-full mt-3"
+                  to="/dashboard "
+                >
+                  My Orders
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className="w-full btn btn-outline rounded-full mt-3"
+                  to="/dashboard/add_review"
+                >
+                  Add A Review
+                </Link>
+              </li>
+            </>
+          )}
+
           <li>
             <Link
               className="w-full btn btn-outline rounded-full mt-3 shadow-xl"
@@ -40,6 +55,26 @@ const Dashboard = () => {
               My Profile
             </Link>
           </li>
+          {admin && (
+            <>
+              <li>
+                <Link
+                  className="w-full btn btn-outline rounded-full mt-3"
+                  to="/dashboard "
+                >
+                  My Orders
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className="w-full btn btn-outline rounded-full mt-3"
+                  to="/dashboard/add_review"
+                >
+                  Add A Review
+                </Link>
+              </li>
+            </>
+          )}
         </ul>
       </div>
     </div>
